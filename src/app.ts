83b5fc24f988917b62d78f0d677c0e1cab317a39
SakumaTakuya <mo-takusan9922@docomo.ne.jp>
2020-07-30 19:03:39 +0900

    install codemirror

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,37 @@
 // BootstrapのJavaScript側の機能を読み込む
-import "bootstrap";
+import 'bootstrap';
+import 'codemirror/mode/markdown/markdown.js';
+import 'codemirror/mode/gfm/gfm.js';
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
 
 import marked from 'marked';
-import hljs from "highlight.js";
+import hljs from 'highlight.js';
+import CodeMirror from 'codemirror';
 
 import {LabeledRenderer} from './renderer';
 import {LatexLabeler} from './label';
-import "./fitarea";
 
 // スタイルシートを読み込む
-import "./index.scss";
+import './index.scss';
 
-hljs.initHighlightingOnLoad();
 
-const edit_area = document.getElementById('markdown_editor_textarea') as HTMLTextAreaElement;
-const preview_area = document.getElementById('markdown_preview');
-if (edit_area != null && preview_area != null) {
-    edit_area.addEventListener('keyup', _ => {
+const textArea = document.getElementById('markdown_editor_textarea') as HTMLTextAreaElement;
+const previewArea = document.getElementById('markdown_preview');
+if (textArea != null && previewArea != null) {
+    const editArea = CodeMirror.fromTextArea(
+        textArea, {
+            mode: {
+                name: "gfm",
+            },
+            lineNumbers: true,
+            lineWrapping : true,
+            theme: "default",
+        });
+    
+    editArea.on('keyup', _ => {
         const labeler = new LatexLabeler(/\\label{(.*?)}/g, /\\ref{(.*?)}/g);
         const renderer = new LabeledRenderer(labeler);
         marked.setOptions({
@@ -34,7 +49,7 @@ if (edit_area != null && preview_area != null) {
             xhtml: false
         });
 
-        const md = marked(edit_area.value);
-        preview_area.innerHTML = labeler.resolveReference(md);
+        const md = marked(editArea.getValue());
+        previewArea.innerHTML = labeler.resolveReference(md);
     });
 }
